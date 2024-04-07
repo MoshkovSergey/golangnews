3a5fceb07ae346818fee0c367a8c49935b2665cc
MoshkovSergey <moshkov_s@vk.com>
2024-04-07 12:44:19 +0300

    added page contects

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ func main() {
 
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello World! %s</h1>", time.Now())
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Hello World! %s</h1><form action=\"/contact\"><button>Контакты</button></form>", time.Now())
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>Контакты</h1><p>Sergey</p><p>Telegram: @sergey</p><a href=\"/\">Назад</a>")
 }
